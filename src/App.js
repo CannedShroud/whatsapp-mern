d10ffefbdee3a1d257eeff713c60d287e04e250a
CannedShroud <hypersync112@gmail.com>
2020-09-13 22:19:05 +0530

    Init Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
+      <h1> WhatsApp MERN stack</h1>
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }
