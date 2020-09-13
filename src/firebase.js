b25ea939a74af3d95f695f3eb78aa62debf26fe4
CannedShroud <hypersync112@gmail.com>
2020-09-13 22:16:03 +0530

    Init firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkxpKcCODb1oAiI5vmEmPqqst8X07mj-M",
+  authDomain: "whatsapp-mern-25b06.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-25b06.firebaseio.com",
+  projectId: "whatsapp-mern-25b06",
+  storageBucket: "whatsapp-mern-25b06.appspot.com",
+  messagingSenderId: "311293831168",
+  appId: "1:311293831168:web:4a8b6adebd5032338dd0e5",
+  measurementId: "G-YRNH4LNTPB",
+};
