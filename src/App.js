b6945cc1717dfe6fe01673c7f7fb4b622f29bffa
CannedShroud <hypersync112@gmail.com>
2020-09-13 22:22:06 +0530

    Init Chat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 function App() {
   return (
     <div className="app">
       <h1> WhatsApp MERN stack</h1>
       <Sidebar />
-      {/* Chat */}
+      <Chat />
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Chat.css";
+
+function Chat() {
+  return (
+    <div classname="chat">
+      {" "}
+      <h1>Me is the Chat Component</h1>
+    </div>
+  );
+}
+
+export default Chat;
