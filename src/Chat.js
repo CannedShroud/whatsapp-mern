1f8afad6c30e28158dfa9a8e04f892abb32b30c5
CannedShroud <hypersync112@gmail.com>
2020-09-14 12:47:37 +0530

    Build Chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,32 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, SearchOutlined, MoreVert } from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 
 function Chat() {
   return (
-    <div classname="chat">
-      <h1>Me is the Chat Component</h1>
+  <div className="chat">
+    <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p></p>
+      </div>
     </div>
   );
 }
