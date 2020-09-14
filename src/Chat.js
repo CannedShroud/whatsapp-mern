6078a80f9f66c9a72f0ad626ea296ff1f783a0ec
CannedShroud <hypersync112@gmail.com>
2020-09-14 19:11:50 +0530

    Build chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,19 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, SearchOutlined, MoreVert } from "@material-ui/icons";
+import {
+  AttachFile,
+  SearchOutlined,
+  MoreVert,
+  InsertEmoticon,
+  Send,
+  Mic,
+} from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 
 function Chat() {
   return (
-  <div className="chat">
-    <div className="chat__header">
+    <div className="chat">
+      <div className="chat__header">
         <Avatar />
         <div className="chat__headerInfo">
           <h3>Room Name</h3>
@@ -25,7 +32,39 @@ function Chat() {
         </div>
       </div>
       <div className="chat__body">
-        <p></p>
+        <div className="chat__message">
+          <p>
+            <span className="chat__name">CannedShroud</span>
+            This is a message
+            <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          </p>
+        </div>
+        <div className="chat__message">
+          <p>
+            <span className="chat__name">CannedShroud</span>
+            This is a message
+            <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          </p>
+        </div>
+        <div className="chat__message chat__reciever">
+          <p>
+            <span className="chat__name">CannedShroud</span>
+            This is a message
+            <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          </p>
+        </div>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <div className="chat__sendIconDiv">
+            <IconButton type="submit">
+              <Send className="chat__sendIcon"/>
+            </IconButton>
+          </div>
+        </form>
+        <Mic />
       </div>
     </div>
   );
