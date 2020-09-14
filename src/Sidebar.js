edcbeec50108216a355529ff0994059da0bb7d5d
CannedShroud <hypersync112@gmail.com>
2020-09-14 10:08:54 +0530

    Build sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,31 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Me is the Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://i.imgur.com/23yg1IZ.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+        <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+            <SearchOutlined />
+            <input type="text" />
+          </div>
+        </div>
     </div>
   );
 }
