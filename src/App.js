c4e4b99588f393c066846de57c3108b97713681e
CannedShroud <hypersync112@gmail.com>
2020-09-15 16:50:14 +0530

    Init and connect to pusher

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("3bdd7e7034a952e266c9", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
